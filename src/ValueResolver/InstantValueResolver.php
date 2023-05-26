1c2034e9ffb0f1de355cfc7c674c3fa9cb524986
Edo Barnas <iam@edobarnas.com>
2023-05-26 11:39:13 +0200

    Create the `Instant` value resolver

---
+++ src/ValueResolver/InstantValueResolver.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Termyn\Symfony\Bundle\ValueResolver;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface as ValueResolver;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Termyn\DateTime\Clock;
+use Termyn\DateTime\Instant;
+
+final readonly class InstantValueResolver implements ValueResolver
+{
+    public function __construct(
+        private Clock $clock,
+    ) {
+
+    }
+
+    public function resolve(
+        Request $request,
+        ArgumentMetadata $argument,
+    ): iterable {
+        if (! $this->supports($argument)) {
+            return [];
+        }
+
+        return [
+            $this->clock->measure(),
+        ];
+    }
+
+    private function supports(ArgumentMetadata $argument): bool
+    {
+        return $argument->getType() && is_a($argument->getType(), Instant::class, true);
+    }
+}
